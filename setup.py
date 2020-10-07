563456e7097a09cec151db469a9ec928c9d80ec4
Lauri Kangassalo <lauri.kangassalo@fmi.fi>
2020-10-07 12:34:04 +0300

    Add pylint

--- setup.py
+++ setup.py
@@ -3,6 +3,8 @@ from setuptools import setup, find_packages
 setup(name="PID-SERVICE",
       install_requires=['requests',
                         'pytest',
+                        'pylint',
+                        'pydantic',
                         'requests_mock',
                         'fastapi',
                         'uvicorn',
